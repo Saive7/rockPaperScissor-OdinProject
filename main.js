23f8c465b80448c1525ce326d3c662d3294add5b
Saive <120703158+Saive7@users.noreply.github.com>
2023-01-31 23:47:23 +0530

    about do some bigger changes so commiting to avoid issues

--- main.js
+++ main.js
@@ -12,8 +12,36 @@ console.log("Hello World");
         return "Scissors"
     }
 }
-let PlayerChoice = ()=> {
-    let playerInput = prompt("enter the choice from Rock Paper Scissors")
-    return playerInput.charAt(0).toUpperCase() + (playerInput.slice(1)).toLowerCase()
 
+    let playerInput = prompt("enter the choice from Rock Paper Scissors")
+    let playerSelection = playerInput.charAt(0).toUpperCase() + (playerInput.slice(1)).toLowerCase()
+
+
+let decision = (playerSelection,computerSelection)=> {
+    if (playerSelection === computerSelection){
+        return `Draw computer choosed ${computerSelection} `
+    }
+    else if (playerSelection=== "Rock" && computerSelection=== "Scissors"){
+        return `You Win! ${playerSelection}  beats ${computerSelection} `
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        return `You Win! ${playerSelection}  beats ${computerSelection} `
+        }
+    else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        return `You Win! ${playerSelection}  beats ${computerSelection} `
+        }
+    else if (computerSelection=== "Rock" && playerSelection=== "Scissors"){
+        return `You Loose! ${computerSelection}  beats ${playerSelection} `
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper"){
+        return `You Loose! ${computerSelection}  beats ${playerSelection} `
+        }
+    else if (computerSelection === "Paper" && playerSelection === "Rock"){
+        return `You Loose! ${computerSelection}  beats ${playerSelection} `
+        }
+
+}
+console.log(decision(playerSelection,computerSelection()))
+function game() {
+    decision()
 }
