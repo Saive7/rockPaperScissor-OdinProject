ff90ba7f576034bc4abfc8a79302628837b332b5
Saive <120703158+Saive7@users.noreply.github.com>
2023-02-01 13:44:50 +0530

    The javascript process of the game is ready

--- main.js
+++ main.js
@@ -1,47 +1,66 @@
-console.log("Hello World");
-//let 0 be rock , 1 be paper annd 2 be the scissor 
- let computerSelection = ()=> {
-    let ComputerChoice = Math.floor(Math.random() * 3) + 1;
-    if (ComputerChoice === 1) {
-        return "Rock"
+let computerChoice = ()=> {
+    let choice = Math.floor(Math.random()*3)+1
+    if (choice === 1) {
+        return `Rock`
+        
     }
-    else if (ComputerChoice === 2){
-        return  "Paper"
-    }
-    else {
-        return "Scissors"
+    else if (choice === 2) {
+        return `Paper`
+    } else if (choice ===3){
+        return `Scissors`
     }
 }
-
-    let playerInput = prompt("enter the choice from Rock Paper Scissors")
-    let playerSelection = playerInput.charAt(0).toUpperCase() + (playerInput.slice(1)).toLowerCase()
-
-
-let decision = (playerSelection,computerSelection)=> {
-    if (playerSelection === computerSelection){
-        return `Draw computer choosed ${computerSelection} `
+//to check if function is working or not
+// console.log(computerChoice());
+let playerChoice = ()=> {
+    let choice = prompt("choose from Rock, Paper, Scissors")
+    let theChoice = (choice.charAt(0).toUpperCase() + (choice.slice(1)).toLowerCase());
+    return theChoice
+}
+//to check the function
+// console.log(playerChoice());
+// the intial scores
+let playerScore = 0;
+let computerScore = 0;
+for (let i = 0; i < 5; i++) {
+ console.log(decision(playerChoice(),computerChoice()))
+    
+}
+function decision(playerChoice,computerChoice) {
+    if (playerChoice === computerChoice) {
+        return `Its a Draw, computer choose ${computerChoice} `
     }
-    else if (playerSelection=== "Rock" && computerSelection=== "Scissors"){
-        return `You Win! ${playerSelection}  beats ${computerSelection} `
-    }
-    else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-        return `You Win! ${playerSelection}  beats ${computerSelection} `
-        }
-    else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        return `You Win! ${playerSelection}  beats ${computerSelection} `
-        }
-    else if (computerSelection=== "Rock" && playerSelection=== "Scissors"){
-        return `You Loose! ${computerSelection}  beats ${playerSelection} `
-    }
-    else if (computerSelection === "Scissors" && playerSelection === "Paper"){
-        return `You Loose! ${computerSelection}  beats ${playerSelection} `
-        }
-    else if (computerSelection === "Paper" && playerSelection === "Rock"){
-        return `You Loose! ${computerSelection}  beats ${playerSelection} `
-        }
+    else if (playerChoice === "Rock" && computerChoice === "Scissors"){
+    playerScore++;
+    return `You Win! Rock beats Scissors`}
+    else if (playerChoice === "Scissors" && computerChoice === "Paper"){
+    playerScore++;
+    return `You Win! Scissors beats Paper`}
+    else if (playerChoice === "Paper" && computerChoice === "Rock"){
+    playerScore++;
+    return `You Win! Paper beats Rock`}
+    else if (computerChoice === "Rock" && playerChoice === "Scissors"){
+    playerScore++;
+    return `You loose! Rock beats Scissors`}
+    else if (computerChoice === "Scissors" && playerChoice === "Paper"){
+    playerScore++;
+    return `You loose! Scissors beats Paper`}
+    else if (computerChoice === "Paper" && playerChoice === "Rock"){
+    playerScore++;
+    return `You loose! Paper beats Rock`}
 
 }
-console.log(decision(playerSelection,computerSelection()))
-function game() {
-    decision()
+let result= ()=> {
+
+    if (playerScore>computerScore) {
+        return `Congo!, You beat the computer and your score is ${playerScore}  `
+        
+    }
+    else if (computerScore>playerScore) {
+        return `Oh!, Computer beated you with score of ${computerScore}`
+    }
+    else if (computerScore === playerScore) {
+        return `The match is draw, Computer is a strong Competetor`
+    }
 }
+console.log(result(playerScore,computerScore))
